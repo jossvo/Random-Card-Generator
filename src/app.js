8f85b2a42d51e82f3a96afae92dfb09dada62543
jossvo <jossvo26@outlook.com>
2022-12-12 20:03:25 +0000

    Final versioion of Remote Card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,62 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
+  changeCardSize();
+};
+
+var timer;
+var cardItem = document.getElementById("card");
+var topCardFigure = document.getElementById("top-figure");
+var bottomCardFigure = document.getElementById("bottom-figure");
+var cardValue = document.getElementById("valor");
+var cardWidthInput = document.getElementById("cardWidthRange");
+var cardHeightInput = document.getElementById("cardHeightRange");
+
+window.generateRandomCard = function generateRandomCard() {
+  let figureList = ["♦", "♥", "♠", "♣"];
+  let fiure = figureList[Math.floor(Math.random() * figureList.length)];
+  topCardFigure.innerText = fiure;
+  bottomCardFigure.innerText = fiure;
+  let cardList = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  cardValue.innerText = cardList[Math.floor(Math.random() * cardList.length)];
+  if (cardItem.classList.contains("red")) cardItem.classList.remove("red");
+  else cardItem.classList.add("red");
+  window.clearTimeout(timer);
+  timer = window.setTimeout(generateRandomCard, 10000);
+};
+window.changeCardSize = function changeCardSize() {
+  cardItem.style.width = cardWidthInput.value + "vw";
+  cardItem.style.height = cardHeightInput.value + "vw";
+  if (cardHeightInput.value < 30.5) {
+    cardValue.style.fontSize = (8 * cardHeightInput.value) / 30.5 + "vw";
+    topCardFigure.style.fontSize = (4.5 * cardHeightInput.value) / 30.5 + "vw";
+    bottomCardFigure.style.fontSize =
+      (4.5 * cardHeightInput.value) / 30.5 + "vw";
+  } else {
+    cardValue.style.fontSize = 8 + "vw";
+    topCardFigure.style.fontSize = 4.5 + "vw";
+    bottomCardFigure.style.fontSize = 4.5 + "vw";
+  }
+};
+window.resetCardSize = function resetCardSize() {
+  cardWidthInput.value = 20.5;
+  cardHeightInput.value = 30.5;
+  topCardFigure.style.fontSize = 4.5 + "vw";
+  bottomCardFigure.style.fontSize = 4.5 + "vw";
+  bottomCardFigure.style.fontSize = changeCardSize();
 };
